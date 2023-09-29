77fdbcce3c9546064ebfdde027a47451a52eaafd
joyalshaji135 <joyalshaji135@gmail.com>
2023-09-29 16:10:10 +0530

    All R  Datastructure

---
+++ Libarays.R
@@ -0,0 +1,28 @@
+# MASS Library
+
+rm(list = ls())
+
+library("MASS")
+
+#AirPassengers Libaray
+
+data()
+data("AirPassengers")
+
+#Boston Library
+
+data("Boston")
+
+View(Boston)
+
+head(Boston)
+
+View(head(Boston))
+
+str(Boston)
+
+View(str(Boston))
+
+summary(Boston)
+
+View(summary(Boston))
