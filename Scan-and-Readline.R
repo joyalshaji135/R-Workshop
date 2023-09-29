93ccd6711f760d95d6d74ebedc401314be502394
joyalshaji135 <joyalshaji135@gmail.com>
2023-09-29 12:21:05 +0530

    Readline Function and scan functions

--- Scan-and-Readline.R
+++ Scan-and-Readline.R
@@ -1,1 +1,21 @@
 #scan function and readline function
+
+rm(list = ls())
+
+#scan function
+
+mylist <- scan()
+
+print(mylist)
+cat(mylist)
+
+#Readline Function
+
+nums=readline(prompt = "Enter the Numbers")
+cat(nums)
+print(nums)
+
+nus = strsplit(mylist,"")
+print(nus)
+
+mystring<-"This is a String function \n "
