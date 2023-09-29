6f548290a68af356cd5487d12bbe433155fb0498
joyalshaji135 <joyalshaji135@gmail.com>
2023-09-29 10:45:11 +0530

    R Variable Declaration

---
+++ Variable.R
@@ -0,0 +1,11 @@
+#logical variable
+
+variable_logical<- TRUE  
+cat(variable_logical,"\n")  
+cat("The data type of variable_logical is ",class(variable_logical),"\n\n")
+
+#numeric variable
+
+variable_numeric<- 3532  
+cat(variable_numeric,"\n")     
+cat("The data type of variable_numeric is ",class(variable_numeric),"\n\n") 
