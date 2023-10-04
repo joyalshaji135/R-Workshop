00fb4c1378d57dffc8b5990f1798994a96824e39
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-04 19:40:29 +0530

    List is Started

--- 03 Data Structure  in R/02-List.R
+++ 03 Data Structure  in R/02-List.R
@@ -1,19 +1,7 @@
-#   List
+# Clear the Environment Terminal
 
 rm(list = ls())
 
-# List type
+# Lists 
 
-#case 1
 
-list1=list(c(2,3,4),21.2,"banana") # collection of a vector
-
-print(list1)
-
-#case 2
-
-vec <- c(3,4,5,6)  
-char_vec<-c("shubham","nishka","gunjan","sumit")  
-logic_vec<-c(TRUE,FALSE,FALSE,TRUE)  
-out_list<-list(vec,char_vec,logic_vec)  
-out_list  
