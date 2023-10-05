7e41921e082f34f569d7b011446eb246a4571af3
joyalshaji135 <joyalshaji135@gmail.com>
2023-10-05 10:09:12 +0530

    Data Visualization is almost completed

---
+++ 04 Data Visualization/03-Boxplot/01-Boxplot.R
@@ -0,0 +1,7 @@
+# Clear the Envoironment Terminal
+
+rm(list = ls())
+
+# boxplot
+
+boxplot(mpg ~ cyl, data = mtcars, xlab = "Quantity of Cylinders", ylab = "Miles Per Gallon", main = "Boxplot Chart - 1 ")  
---
+++ 04 Data Visualization/04-Histogram/01-Histogram.R
@@ -0,0 +1,9 @@
+# Clear the Environment Terminal
+
+rm(list = ls())
+
+# Histogram
+
+v <-  c(12,24,16,38,21,13,55,17,39,10,60)  
+
+hist(v,xlab = "Weight",ylab="Frequency",col = "green",border = "red")  
